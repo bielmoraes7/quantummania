60d08c28d5ca936ba2c27f35fe6b7fdd2488386e
moraes <gabigol2016.amr@gmail.com>
2024-03-22 11:16:00 -0300

    adjust target

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
   // pegar a tag img
   const img = document.querySelector("#profile img")
-
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
@@ -12,5 +10,5 @@ function toggleMode() {
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/assets/Avatar.png")
-  }
+  }   
 }
